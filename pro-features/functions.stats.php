3f2e76e1e931529014503d43f7c155c937a4ef81
Ali YeKen <email@yeken.uk>
2016-11-27 18:48:15 +0000

    Remove IDs from stats that that have no weight entries

--- pro-features/functions.stats.php
+++ pro-features/functions.stats.php
@@ -12,6 +12,9 @@ function ws_ls_stats_run_cron() {
 	// Copy across anyone missing from the stats table!
 	ws_ls_stats_insert_missing_user_ids_into_stats();
 
+	// Remove any old IDs from stats table
+	ws_ls_stats_remove_deleted_user_ids_from_stats();
+
 	// Fetch some records to process
 	if($users = ws_ls_stats_fetch_those_that_need_update()) {
 		foreach ($users as $user) {
@@ -85,51 +88,5 @@ function ws_ls_stats_update_for_user($user_id) {
 		ws_ls_stats_refresh_summary_stats();
 	}
 
-	return;
-}
-
-/*
-	Fetch records that haven't been updated in the last hour
-*/
-function ws_ls_stats_fetch_those_that_need_update($max = 100) {
-
-	global $wpdb;
-	$table_name = $wpdb->prefix . WE_LS_USER_STATS_TABLENAME;
-	$sql = 'SELECT * FROM ' . $table_name . ' where last_update < DATE_SUB(NOW(),INTERVAL 6 HOUR) or last_update is null ORDER BY RAND() ';
-	$rows = $wpdb->get_results( $sql, ARRAY_A );
-
-	if (is_array($rows) && count($rows) > 0) {
-		return $rows;
-	}
-
-	return false;
-}
-
-/*
-	Refresh total lost count
-*/
-function ws_ls_stats_sum_weight_difference() {
-
-	global $wpdb;
-	$result = $wpdb->get_var( 'SELECT sum(weight_difference) FROM ' . $wpdb->prefix . WE_LS_USER_STATS_TABLENAME );
-
-	if (!is_null($result)) {
-		return floatval($result);
-	}
-
-	return false;
-}
-
-/*
-	Copy user IDs of those that have entered weights into stats table (assuming they aren't they're already)
-*/
-function ws_ls_stats_insert_missing_user_ids_into_stats() {
-
-	global $wpdb;
-	$stats_table_name = $wpdb->prefix . WE_LS_USER_STATS_TABLENAME;
-	$data_table_name = $wpdb->prefix . WE_LS_TABLENAME;
-	$sql = "INSERT INTO $stats_table_name (user_id, start_weight, recent_weight, weight_difference, last_update)
-			Select distinct weight_user_id, NULL, NULL, NULL, NULL from $data_table_name where weight_user_id not in (Select user_id from $stats_table_name)";
-	$wpdb->query($sql);
 	return;
 }
