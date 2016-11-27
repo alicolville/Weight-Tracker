866a4002ecde5bdb8a657005744399670277c88f
Ali YeKen <email@yeken.uk>
2016-11-27 19:56:08 +0000

    Added invert and display types for stats shortcode

--- pro-features/shortcode-stats.php
+++ pro-features/shortcode-stats.php
@@ -5,22 +5,40 @@ defined('ABSPATH') or die("Jog on!");
 function ws_ls_shortcode_stats_total_lost($user_defined_arguments)
 {
     if(!WS_LS_IS_PRO || WE_LS_DISABLE_USER_STATS) {
-       return false;
+       return  __('Stats disabled', WE_LS_SLUG);
     }
 
+	$arguments = shortcode_atts(
+	array(
+		'display' => 'number/text',
+		'invert' => false
+	 ), $user_defined_arguments );
+
 	$difference = ws_ls_stats_get_summary_stats();
-	$stats = ['kg' => $difference, 'display-unit' => ws_ls_get_config('WE_LS_DATA_UNITS')];
+	$stats = ['kg' => $difference, 'display-unit' => ws_ls_get_config('WE_LS_DATA_UNITS'), 'display-value' => ''];
+
+	// If display number text, remove sign and use text to represent gain / loss
+	if('number/text' == $arguments['display']) {
+
+		$stats['display-value'] = ($difference <= 0) ? __('Lost', WE_LS_SLUG) : __('Gained', WE_LS_SLUG);
+		$stats['display-value'] .= ': ';
+		$difference = abs($difference);
+
+	} else {
+		// Invert positive / negative numbers
+		$difference = (false === ws_ls_force_bool_argument($arguments['invert'])) ? $difference : -$difference ;
+	}
 
 	switch (ws_ls_get_config('WE_LS_DATA_UNITS')) {
 		case 'pounds_only':
-			$stats['display-value'] = ws_ls_to_lb($difference) .  __('lbs', WE_LS_SLUG);
+			$stats['display-value'] .= ws_ls_to_lb($difference) .  __('lbs', WE_LS_SLUG);
 			break;
 		case 'stones_pounds':
 			$weight = ws_ls_to_stone_pounds($difference);
-			$stats['display-value'] = $weight['stones'] . __('St', WE_LS_SLUG) . " " . $weight['pounds'] . __('lbs', WE_LS_SLUG);
+			$stats['display-value'] .= $weight['stones'] . __('St', WE_LS_SLUG) . " " . $weight['pounds'] . __('lbs', WE_LS_SLUG);
 			break;
 		default:
-			$stats['display-value'] = $difference  . __('Kg', WE_LS_SLUG);
+			$stats['display-value'] .= $difference  . __('Kg', WE_LS_SLUG);
 	}
 
 	// Allow theme developer to override stats message
