0ae71b109eaf05890b7c21eb2bc906d969e69e4f
Al YeKen <email@yeken.uk>
2018-09-16 19:10:09 +0100

    6.0.1

--- weight-loss-tracker.php
+++ weight-loss-tracker.php
@@ -5,7 +5,7 @@ defined('ABSPATH') or die('Jog on!');
 /**
  * Plugin Name: Weight Loss Tracker
  * Description: Allow registered users of your website to track their weight and relevant body measurements. History can be displayed in both tables & charts.
- * Version: 6.0
+ * Version: 6.0.1
  * Author: YeKen
  * Author URI: https://www.YeKen.uk
  * License: GPL2
@@ -28,8 +28,8 @@ defined('ABSPATH') or die('Jog on!');
 */
 
     define('WS_LS_ABSPATH', plugin_dir_path( __FILE__ ));
-    define('WE_LS_CURRENT_VERSION', '6.0');
-    define('WE_LS_DB_VERSION', '6.0');
+    define('WE_LS_CURRENT_VERSION', '6.0.1');
+    define('WE_LS_DB_VERSION', '6.0.1');
 
 // -----------------------------------------------------------------------------------------
 // AC: Activate / Deactivate / Uninstall Hooks
