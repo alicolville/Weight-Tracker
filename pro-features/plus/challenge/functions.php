474658bf40e5aece0c6be3f16f45c3eb6ac4bfd1
Al <email@yeken.uk>
2019-12-04 16:57:00 +0000

    Refactor user height function

--- pro-features/plus/challenge/functions.php
+++ pro-features/plus/challenge/functions.php
@@ -21,11 +21,28 @@ function ws_ls_challenges_data_update_row( $user_id, $challenge_id ) {
         return;
     }
 
+    $weight_entries = ws_ls_challenges_get_weight_entries( $user_id, $challenge[ 'start_date' ], $challenge[ 'end_date' ] );
+
+    $data = []; $formats = [ '%d', '%f', '%f' ];
+
+    // Weight Data
+    $data[ 'count_wt_entries' ] = count( $weight_entries );
+    $data[ 'weight_start' ]     = $weight_entries[ 0 ][ 'kg' ];
+    $data[ 'weight_latest' ]    = $weight_entries[ $data[ 'count_wt_entries' ] - 1 ][ 'kg' ];
+
+    // BMI
+    $user_height = ws_ls_get_user_height( $user_id );
+
+    var_dump($user_height);
+
+    //ws_ls_calculate_bmi($cm, $kg)
+
+    print_r($data);
+
     /*
      * TODO:
      *
-     * 1) Fetch all weight entries for this user within the given time period of the challenge
-     * 2) Fetch start and end weight. Calculate difference.
+
      * 3) Fetch the user's preferences. Using start and end weight, calculate starting BMI, ending BMI and difference.
      * 4) Count Weight Entries for given period
      * 5) Fetch and count Meal Tracker entries for this period
@@ -36,8 +53,12 @@ function ws_ls_challenges_data_update_row( $user_id, $challenge_id ) {
 
 function t() {
 
-    $t = ws_ls_challenges_data_update_row( 101,22 );
-  //  print_r( $t );
+    ws_ls_challenges_add( 708, '2019-08-01', '2019-08-28' );
+
+    // ws_ls_challenges_identify_entries( 708, '2019-08-01', '2019-08-28' );
+
+    $t = ws_ls_challenges_data_update_row( 1,708 );
+    print_r( $t );
     die;
 }
 add_action( 'init', 't' );
