5e8c5d97d58edb33dc566df03fb6d4e4a5b597c4
Al <email@yeken.uk>
2020-06-06 18:31:18 +0100

    Update weight-loss-tracker.php

--- weight-loss-tracker.php
+++ weight-loss-tracker.php
@@ -28,8 +28,8 @@ defined('ABSPATH') or die('Jog on!');
 */
 
     define('WS_LS_ABSPATH', plugin_dir_path( __FILE__ ));
-    define('WE_LS_CURRENT_VERSION', '7.7');
-    define('WE_LS_DB_VERSION', '7.7');
+    define('WE_LS_CURRENT_VERSION', '7.7-beta1');
+    define('WE_LS_DB_VERSION', '7.7-beta1');
 
 // -----------------------------------------------------------------------------------------
 // AC: Activate / Deactivate / Uninstall Hooks
