3ea096621e642236d5a0b27530a0bb0934fb3d43
Al <email@yeken.uk>
2020-06-11 20:26:48 +0100

    Refactoring

--- pro-features/functions-stats.php
+++ pro-features/functions-stats.php
@@ -26,7 +26,7 @@ function ws_ls_stats_run_cron() {
 
 	ws_ls_stats_refresh_summary_stats();
 }
-add_action( WE_LS_CRON_NAME , 'ws_ls_stats_run_cron');
+add_action( 'weight_loss_tracker_hourly' , 'ws_ls_stats_run_cron');
 add_action( 'wlt-hook-data-all-deleted', 'ws_ls_stats_run_cron' );	// Delete stats if all user data has been deleted
 add_action( 'wlt-hook-data-user-deleted', 'ws_ls_stats_run_cron' );	// Tidy up stats if a user deletes their entry
 
