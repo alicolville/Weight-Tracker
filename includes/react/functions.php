bea688cfa1661eeb0bd05a1a75381d215ebabb48
Ali Colville <acolville@dctmedia.co.uk>
2020-11-17 11:31:42 +0000

    Added React config

--- includes/react/functions.php
+++ includes/react/functions.php
@@ -9,4 +9,34 @@ function ws_ls_react_enqueue() {
 	$version = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? mt_rand() : WE_LS_CURRENT_VERSION;
 
 	wp_enqueue_script( 'yk-wt-react', plugins_url( 'react/frontend/assets/js/public.min.js', __DIR__ ), [ 'jquery' ], $version, true );
+
+	wp_localize_script( 'yk-wt-react', 'ws_ls_react', ws_ls_react_config() );
+}
+
+/**
+ * Return an array of settings for React App
+ * @return mixed|void
+ */
+function ws_ls_react_config() {
+
+	$config = [
+				'locale' => ws_ls_react_config_locale()
+
+	];
+
+	return apply_filters( 'wlt-filter-react-config', $config );
+}
+
+/**
+ * Return a array of strings to use in React App
+ * @return mixed|void
+ */
+function ws_ls_react_config_locale() {
+
+	$strings = [
+		'target' => __( 'Target', WE_LS_SLUG )
+
+	];
+
+	return apply_filters( 'wlt-filter-react-config-locale', $strings );
 }
