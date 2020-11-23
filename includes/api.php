9cf7fea24922a6f0ad77e9be94bb3aabb28ef611
Ali Colville <email@yeken.uk>
2020-11-23 20:20:33 +0000

    Added Target Set

--- includes/api.php
+++ includes/api.php
@@ -14,6 +14,11 @@ add_action( 'rest_api_init', function () {
 																										'permission_callback' 	=> 'ws_ls_api_permission_check',
 																									],
 																									[
+																										'methods' 				=> WP_REST_Server::EDITABLE,
+																										'callback' 				=> 'ws_ls_api_target_set',
+																										'permission_callback' 	=> 'ws_ls_api_permission_check',
+																									],
+																									[
 																										'methods' 				=> WP_REST_Server::DELETABLE,
 																										'callback' 				=> 'ws_ls_api_target_delete',
 																										'permission_callback' 	=> 'ws_ls_api_permission_check',
@@ -41,12 +46,46 @@ function ws_ls_api_target_get() {
 	return ws_ls_target_get( get_current_user_id() );
 }
 
-function ws_ls_api_target_set() {
+/**
+ * Take a new Target weight and update database
+ * @param $request
+ * @return float|mixed|null
+ */
+function ws_ls_api_target_set( $request ) {
 
-	//$param = $request['some_param'];
+	$kg = NULL;
 
-	// Or via the helper method:
-	//$param = $request->get_param('some_param');
+	// Are we lucky? Metric by default?
+	if ( true === isset( $request[ 'kg' ] ) ) {
+		$kg = $request[ 'kg' ];
+	}
+
+	// Imperial?
+	if ( NULL === $kg ) {
+
+		$stones = ( true === isset( $request[ 'stones' ] ) ) ? $request[ 'stones' ] : NULL;
+		$pounds = ( true === isset( $request[ 'pounds' ] ) ) ? $request[ 'pounds' ] : NULL;
+
+		// Stones and Pounds
+		if ( NULL !== $stones ) {
+
+			// Force pounds to zero if not specified
+			$pounds = ( true === empty( $pounds ) ) ? 0 : $pounds;
+
+			$kg = ws_ls_convert_stones_pounds_to_kg( $stones, $pounds );
+
+		} elseif ( NULL !== $pounds ) {
+
+			$kg = ws_ls_convert_pounds_to_kg( $pounds );
+
+		}
+	}
+
+	if ( NULL === $kg ) {
+		return false;
+	}
+
+	return ws_ls_db_target_set( get_current_user_id(), $kg );
 }
 
 /**
