e6a21a0df9606cfb5e10075b7a6f383c8663c981
Ali Colville <email@yeken.uk>
2020-11-27 19:04:15 +0000

    Tidy up debug

--- includes/react/frontend/src/index.js
+++ includes/react/frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import WeightTracker from './components/WeightTracker';
-console.log('here');
-render(
 
+render(
   <div>
     <WeightTracker />
   </div>
