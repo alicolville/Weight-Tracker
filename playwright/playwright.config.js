8906af598a65d7dbd86331d865bc99ebac768e2f
Ali Colville <email@yeken.uk>
2024-04-18 14:09:48 +0100

    Simplified config

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -1,44 +1,22 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: 'tests',
-
-  // Run all tests in parallel.
-  fullyParallel: true,
-
-  // Fail the build on CI if you accidentally left test.only in the source code.
-  forbidOnly: !!process.env.CI,
-
-  // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
-
-  // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
-
-  // Reporter to use
-  reporter: 'html',
-
-  use: {
-    // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://localhost',
-
-    // Collect trace when retrying the failed test.
-    trace: 'on-first-retry',
-  },
-  // Configure projects for major browsers.
   projects: [
-    { name: 'auth', testMatch: /.*\.auth\.js/ },
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-      dependencies: ['auth'],
+      name: "auth",
+      testMatch: /.*\.auth\.js/
+    },
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: ".auth/user.json"
+      },
+      testMatch: /.*\.test*\.js/,
+      dependencies: ["auth"],
     },
   ],
-  // Run your local dev server before starting the tests.
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost',
-    reuseExistingServer: !process.env.CI,
-  },
+  reporter: "html",
+  testDir: "tests",
+  timeout: 5000,
 });
