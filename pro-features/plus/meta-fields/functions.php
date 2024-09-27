42dae4c3e53ea9d90dff87a094e8a3d833341e82
Ali Colville <email@yeken.uk>
2024-09-27 14:13:59 +0100

    Removed older file types

--- pro-features/plus/meta-fields/functions.php
+++ pro-features/plus/meta-fields/functions.php
@@ -589,7 +589,7 @@ function ws_ls_meta_fields_form_field_select( $field, $value ) {
 
         // Show Add button
         $html .= sprintf('<div class="ws-ls-cell ws-ls-photo-select">
-                                <input type="file" data-msg="%6$s \'%7$s\'." name="%1$s" id="%8$s" tabindex="%2$s"data-rule-accept="image/jpeg,image/pjpeg,image/png,image/gif,image/bmp,image/tiff"
+                                <input type="file" data-msg="%6$s \'%7$s\'." name="%1$s" id="%8$s" tabindex="%2$s"data-rule-accept="image/jpeg,image/pjpeg,image/png"
                                     class="ws-ls-hide ws-ls-input-file ws-ls-meta-fields-photo" %5$s data-required="%4$s" />
                                 <label for="%8$s" class="ws-ls-button">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg>
