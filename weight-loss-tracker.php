be16b7662c003ff92a0c84ef6b247e0f83f389d7
Ali Colville <email@yeken.uk>
2024-10-09 15:53:11 +0100

    Bug fix: Fixed an issue where user preferences wouldn't submit if a slide field was on the from.

--- weight-loss-tracker.php
+++ weight-loss-tracker.php
@@ -5,7 +5,7 @@ defined('ABSPATH') or die('Jog on!');
 /**
  * Plugin Name:         Weight Tracker
  * Description:         Allow your users to track their weight, body measurements, photos and other pieces of custom data. Display in charts, tables, shortcodes and widgets. Manage their data, issue awards, email notifications, etc! Provide advanced data on Body Mass Index (BMI), Basal Metabolic Rate (BMR), Calorie intake, Harris Benedict Formula, Macronutrients Calculator and more.
- * Version:             10.14
+ * Version:             10.15
  * Requires at least:   6.0
  * Tested up to:		6.5
  * Requires PHP:        7.4
@@ -17,7 +17,7 @@ defined('ABSPATH') or die('Jog on!');
  * Domain Path:         /includes/languages
  */
 
-define( 'WE_LS_CURRENT_VERSION', '10.14' );
+define( 'WE_LS_CURRENT_VERSION', '10.15' );
 define( 'WS_LS_ABSPATH', plugin_dir_path( __FILE__ ) );
 define( 'WS_LS_BASE_URL', plugin_dir_url( __FILE__ ) );
 define( 'WE_LS_TITLE', 'Weight Tracker' );
